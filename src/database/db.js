73d369d28f150ae3df8c04a7bd078f55b3e01584
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-09-14 20:25:20 -0300

    intalamos mongoose

---
+++ src/database/db.js
@@ -0,0 +1,25 @@
+const uri = process.env.PORTDB;
+const db = process.env.DB;
+
+
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(`${uri}/${db}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log("DB esta conectado");
+
+    const allHabitaciones = await HabitacionModel.find(); //buscamos todos los documentos de la coleccion
+    console.log(allHabitaciones);
+
+    const allUsuarios = await UsuarioModel.find(); //buscamos todos los documentos de la coleccion
+    console.log(allUsuarios);
+
+
+  }
+
+  catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDb;
