bb48ef475cfb49315aadc5afeb81b0e2d482574e
nachoing <juaning2023@gmail.com>
2023-10-07 20:22:01 -0300

    arregle problemas de conexion con base de datos

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const HabitacionModel = require("../models/Habitacion");
-const UsuarioModel = require("../models/Usuario.JS");
+
 
 const uri = process.env.PORTDB;
 const db = process.env.DB;
