9e59bc6b3196619f15e08360fc28e26b7d909964
nachoing <juaning2023@gmail.com>
2023-10-07 21:45:01 -0300

    agregue rutas al index

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-import express from "express";
+// src/index.js
+import express from 'express';
 import 'dotenv/config';
-import cors from "cors";
-import morgan from "morgan";
+import cors from 'cors';
+import morgan from 'morgan';
 
 // Cambia la importación de connectDb para que utilice import en lugar de require
-import connectDb from "./src/database/db";
+import connectDb from './src/database/db';
 
-console.log("Hello World BACK END 46i");
+// Importa las rutas de usuarios
+import userRoutes from './src/routes/userRoutes';
+
+console.log('Hello World BACK END 46i');
 
 // Crear una instancia de express
 const app = express();
 
 // Configurar el puerto en el que se va a ejecutar el servidor
-app.set("port", process.env.PORT);
+app.set('port', process.env.PORT);
 
 // Conectar a la base de datos
 const initApp = async () => {
   try {
     await connectDb();
-    app.listen(app.get("port"), () => {
-      console.log(`BackEnd46i listening to PORT: ${app.get("port")}`);
-    }).on("error", (error) => {
-      console.log("ERROR:", error);
+    app.listen(app.get('port'), () => {
+      console.log(`BackEnd46i listening to PORT: ${app.get('port')}`);
+    }).on('error', (error) => {
+      console.log('ERROR:', error);
       process.exit(1);
     });
   } catch (error) {
-    console.log("ERROR:", error);
+    console.log('ERROR:', error);
     process.exit(1);
   }
 };
@@ -34,4 +38,17 @@ initApp();
 
 // Middlewares
 app.use(express.json());
+
+// Habilita CORS
+app.use(cors());
+
+// Muestra los logs en la consola
+app.use(morgan('dev'));
+
+// Rutas
+app.use('/api/users', userRoutes);
+
+
+export default app;
+
 
