7d25d0d2411c727022d8d9e84b436c0e35e23e0b
nachoing <juaning2023@gmail.com>
2023-10-07 21:58:19 -0300

    hice cambios en el db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,23 +1,14 @@
 const mongoose = require('mongoose');
 
-
-const uri = process.env.PORTDB;
-const db = process.env.DB;
-
-
+const uri = process.env.PORTDB; // La URL completa, que ya incluye el nombre de la base de datos
+// No es necesario utilizar process.env.DB aquí
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(`${uri}/${db}`, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log("DB esta conectado");
-
-   
-
-
-  }
-
-  catch (error) {
-    console.log(error);
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("DB está conectada");
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
   }
 };
 
