3debf143040d21b5441c45cd46f69d1f542ff0c2
nachoing <juaning2023@gmail.com>
2023-10-18 22:50:01 -0300

    se conecto a la base de datos sgh

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,16 @@
+// src/database/db.js
+
 const mongoose = require('mongoose');
 
-const uri = process.env.PORTDB; // La URL completa, que ya incluye el nombre de la base de datos
-// No es necesario utilizar process.env.DB aquí
+const uri = process.env.PORTDB;
+const db = process.env.DB || 'SGH'; // Utiliza 'SGH' como valor predeterminado si DB no está definido en el archivo .env
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(`${uri}/${db}`, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("DB está conectada");
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+    console.log(error);
   }
 };
 
